713173e5d21266a4e8bff0c45cb4f0189418ea46
dika firmansyah <diikafirmansyah94@gmail.com>
2024-05-24 23:40:19 +0900

    final

--- src/dashboard/Dashboar.test.tsx
+++ src/dashboard/Dashboar.test.tsx
@@ -1,18 +1,43 @@
-import {render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Dashboard from "./Dashboard";
 import userEvent from "@testing-library/user-event";
 
-describe ("Dashboar Component", () => {
-    test ("Renders Dashboard Component", () => {
-        render(<Dashboard/>);
-        
-        const formElement = screen.queryByRole ("form", {
+describe("Dashboar Component", () => {
+  test("Renders Dashboard Component", () => {
+    render(<Dashboard />);
 
-        })
-        expect (formElement).toBeDefined();
+    const formElement = screen.queryByRole("form", {});
+    expect(formElement).toBeDefined();
 
-        const buttonElement  = screen.getByRole("button", {name : "Submit" });
-        userEvent.click (buttonElement);
-        expect(buttonElement).toBeTruthy();
-    })
-})
+    const buttonElement = screen.getByRole("button", { name: "Submit" });
+    userEvent.click(buttonElement);
+    expect(buttonElement).toBeTruthy();
+
+    const inputElement = screen.getByPlaceholderText("Pokemon Name");
+    expect(inputElement).toBeDefined();
+  });
+
+  test("submit button for search pokemon corectly", () => {
+    render(<Dashboard />);
+
+    const pokemonInputName = screen.getByPlaceholderText("Pokemon Name");
+    userEvent.type(pokemonInputName, "arbok");
+    expect(pokemonInputName).toBeTruthy;
+
+    const submitButtonElement = screen.getByRole("button", { name: "Submit" });
+    userEvent.click(submitButtonElement);
+    expect(submitButtonElement).toBeTruthy();
+  });
+  test("fetching data debounced", async () => {
+    render(<Dashboard />);
+
+    const input = screen.getByPlaceholderText("Pokemon Name");
+    userEvent.type(input, "zubat");
+
+    const submitButton = await screen.findByText("Submit");
+
+    userEvent.click(submitButton);
+
+    
+  });
+});
